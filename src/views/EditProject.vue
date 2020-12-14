5dcffea4ea704538108fc9fe65d4b137c3ddc1d4
HlaingMinThan <hlaingminthan92@gmail.com>
2020-12-14 19:56:27 +0630

    update project backend

--- src/views/EditProject.vue
+++ src/views/EditProject.vue
@@ -5,7 +5,7 @@
       <input type="text" v-model="title">
       <label>Project Detail</label>
       <input type="text" v-model="detail">
-      <button>update Project</button>
+      <button @click="updateProject">update Project</button>
   </form>
 </template>
 
@@ -32,7 +32,30 @@ export default {
     .catch((err)=>{
       console.log(err);
     })
-  }
+  },
+  methods:{
+    updateProject(){
+        fetch('http://localhost:3000/projects/'+this.id,{
+          method:"PATCH",
+          headers:{
+            "Content-type":"application/json"
+          },
+          body:JSON.stringify(
+            {
+              title:this.title,
+              detail:this.detail
+            }
+          )
+        })
+        .then(()=>{
+            this.$router.push("/")
+        })
+        .catch((err)=>{
+          console.log(err);
+        })
+      
+    }
+  } 
 }
 </script>
 
